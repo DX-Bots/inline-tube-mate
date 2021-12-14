f3b2a19f3815aee982ce431610e1d3de3f0953d2
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-14 13:45:06 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,58 +1,58 @@
 class Presets(object):
     HELP_TEXT = """
-<b><u>Inline Tube Mate Help:</u></b>
+<b><u>How to Use The Bot with Inline:</u></b>
 <code>
-🔰 Send any image to set up a permanent thumbnail for the downloading videos.
+➡ Send any image to set up a permanent thumbnail for the downloading videos.
 
-🔰 To delete the previously set thumbnail, select the</code> <b>/help</b> <code>and click on the delete button.
+➡ To delete the previously set thumbnail, select the</code> <b>/help</b> <code>and click on the delete button.
 
-🔰 If no custom thumbnails are available, the default video thumbnail will be used in the downloading videos.
+➡ If no custom thumbnails are available, the default video thumbnail will be used in the downloading videos.
 
-🔰 Search Inline or paste any YouTube URL to start the bot activity.
+➡ Search Inline or paste any YouTube URL to start the bot activity.
 
-🔰 SUDO USERS can broadcast the messages to all the users of the bot. Use:</code>
+➡ SUDO USERS can broadcast the messages to all the users of the bot. Use:</code>
 <b>/subs</b><code> - to count subscribers</code>
 <b>/send</b><code>  - to broadcast (as reply)</code>
 
-<b>Join:</b><a href='https://t.me/RMProjects'><b> Support</b></a> | Repo\
-<a href='https://github.com/m4mallu/inline-tube-mate'><b> LINK</b></a>   
+<b>Join:</b><a href='https://t.me/TeleRoidGroup'><b>@TheTeleRoidGroup</b></a> | Repo\
+<a href='https://github.com/PredatorHackerzZ'><b> LINK</b></a>   
     """
 
-    OPTIONS_TXT = "<code>🔰 Select the option 🔰</code>"
-    WELCOME_MSG = "<code>Hello...</code><b>{}</b> 👋\n<code>I'm a YouTube downloader bot with lot's of features. " \
+    OPTIONS_TXT = "<code>⭕ Select the option ⭕</code>"
+    WELCOME_MSG = "<code>Hello...</code><b>{}</b> 👋\n<code>I'm a YouTube Inline Downloader Bot with Thumbnail Support." \
                   "Search inline for videos and click to proceed for download.</code>"
-    RESULTS_TXT = "👀 Results:"
-    NO_RESULTS = "❌ No Results"
+    RESULTS_TXT = "Results Found 👀:"
+    NO_RESULTS = "❌ No Results Found for This"
     DESCRIPTION = "Duration: {} || {}"
     NOT_AUTH_TXT = "<b>Error : </b>\n\n<code>You are not Authorized to use this bot.</code>"
-    DEFAULT_TITLE = "Inline Tube Mate Repository"
+    DEFAULT_TITLE = "Aryan-Vikash YouTube Repository"
     DEFAULT_THUMB_URL = "https://image.flaticon.com/icons/png/512/25/25231.png"
-    DEFAULT_LINK = "https://github.com/m4mallu/inline-tube-mate"
-    DEFAULT_DESCRIPTION = "Link: M4mallu | GitHub"
-    DEV_TITLE = "Developer Information"
+    DEFAULT_LINK = "https://t.me/MoviesFlixers_DL"
+    DEFAULT_DESCRIPTION = "Link: PredatorHackerzZ | GitHub"
+    DEV_TITLE = "Developer Of The Bot"
     DEV_THUMB_URL = "https://freepikpsd.com/media/2019/10/software-developer-icon-png-2-Transparent-Images.png"
-    DEV_LINK = "https://t.me/space4renjith"
-    DEV_DESCRIPTION = "Name: Renjith Mangal | Telegram"
-    SHARE_BUTTON_TEXT = "Hi.. 👋\nCheckout : @{username}\nFor search and download TouTube Videos"
-    SAVED_THUMB = "<b>✅ Thumbnail Saved Successfully</b>\n<code>This file will be used in next YouTube " \
+    DEV_LINK = "https://t.me/TheTeleRoid"
+    DEV_DESCRIPTION = "Name: PredatorHackerzZ | Instagram"
+    SHARE_BUTTON_TEXT = "Heya.. 👋\nCheckout : @{username}\nFor search and download TouTube Videos"
+    SAVED_THUMB = "<b>✅ Thumbnail Saved Successfully</b>\n<code>This file will be used in next YouTube Link." \
                   "downloads until you clear it !</code> "
     WAIT_MESSAGE = "<code>Processing...</code>"
     THUMB_CAPTION = "<code>This image is your current thumbnail, Tap </code><b> DEL THUMB </b><code> if you wish to " \
                     "clear it !</code> "
-    NO_THUMB = "❌ Can't find any thumbnail in your local, Please upload an image to set it.."
+    NO_THUMB = " Can't find any saved thumbnail, Please upload an image to set it.."
     DEL_THUMB_CNF = "Thumbnail Cleared Successfully ✅"
     LINK_ERROR = "<b>Error : </b>\n\n<code>Some error occurred while the process !\n Plz try again later..</code>"
     NO_VOID_FORMAT_FOUND = "<code>{}</code>"
     FINISHED_PROGRESS_STR = "◼️"
     UN_FINISHED_PROGRESS_STR = "◻️"
     CHECKING_LINK = "⏳ <code>Analyzing your link...</code>"
-    DOWNLOAD_START = "⬇️ <code>Download Initiated...</code>"
-    UPLOAD_START = "⬆️ <code>Upload Initiated...</code>"
-    NOT_DOWNLOADABLE = "<b>This URL is not downloadable !</b> 🙄"
+    DOWNLOAD_START = "📥 <code>Downloading Your Given YouTube Link...</code>"
+    UPLOAD_START = "📤 <code>Uploading Your Given YouTube Link...</code>"
+    NOT_DOWNLOADABLE = "<b>This URL is not downloadable In This Bot!</b> 🙄"
     CANCEL_PROCESS = "<b>Process Cancelled Successfully</b>  ✅"
     SEND_TEXT = "<b>Processing...</b>\n<i>This message will automatically disappear when the broadcasting is " \
                 "finished</i> "
-    REPLY_ERROR = "<i>Use this command as a replay to any telegram message with out any spaces.</i>"
+    REPLY_ERROR = "<i>Use this command as a replay to any telegram message without any spaces.</i>"
     USERS_LIST = "<b>Total: {}</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Processing...</b>\n<i>This will take some time...</i>"
     PROMPT_THUMB = "<b>Do you want to set this image as a thumbnail?</b>"
@@ -65,18 +65,18 @@ class Presets(object):
 
 👇<code>Select the required format</code>👇
     """
-    NOT_SUB_TXT = "<b>Oopz </b> 😯\n\n<code>In order to use this feature, You need to join my channel.</code>"
-    BOT_NOT_PRESENT = "<b>Error : </b>\n\n<code>Bot need to be an admin to the force subscribe chat !</code>"
+    NOT_SUB_TXT = "<b>Oopz </b> 😯\n\n<code>You need to join my channel to use This Bot.</code>"
+    BOT_NOT_PRESENT = "<b>Error : </b>\n\n<code>Something Went Wrong. Report at Support Group @TeleRoid14!</code>"
     NO_INVITE_METHOD = "<b>Error : </b>\n\n<code>Chat invite method not found ! Try to generate an invite " \
                        "link in your force subscribe chat.</code>"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | newfilename | username | password"""
     CUSTOM_CAPTION_UL_FILE = "\xad \xad\n<b>{}</b>\n\n<b>DL Credits: </b><b><a " \
-                             "href='https://github.com/m4mallu'> M4Mallu</a></b> "
+                             "href='https://github.com/PredatorHackerzZ'>Predator</a></b> "
     RCHD_TG_API_LIMIT = "Detected File Size: {}\n\nSorry. But, I cannot upload files " \
                         "greater than 1.95GB due to Telegram API limitations."
     AD_STRING_TO_REPLACE = "please report this issue on https://yt-dl.org/bug . Make sure you are using the " \
                            "latest version; see  https://yt-dl.org/update  on how to update. Be sure to call " \
                            "youtube-dl with the --verbose flag and include its complete output."
-    INITIAL_MEDIA = "https://telegra.ph/file/f328510aa16bb40a4cd03.gif"
+    INITIAL_MEDIA = "https://telegra.ph/file/e9ac57ceab6a8cacdb562.jpg"
     SETTINGS = "<code>Need an update in settings?</code>"
